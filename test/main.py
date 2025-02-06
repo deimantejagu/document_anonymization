3c154fe8a7be8419b5083ff5f31decd709d25deb
deimantejagu <deimajagu@outlook.com>
2025-02-06 11:39:44 +0200

    random names generator

--- test/main.py
+++ test/main.py
@@ -1,5 +1,4 @@
 import re
-import random
 from faker import Faker
 
 fake = Faker('lt_LT')
@@ -12,21 +11,27 @@ FEMALE_LAST_NAME_ENDINGS = ['aitė', 'ytė', 'utė', 'iūtė', 'ūtė', 'ė', 'i
 
 NAMES = []
 
-i = 0
-while i < 10:
-    first_name = fake.first_name() 
+def generate_names(endings_array, first_name):
     last_name = fake.last_name()  
+    last_name_base = re.sub(r'(' + '|'.join(MALE_LAST_NAME_ENDINGS) + r')$', '', last_name)
+    initials = re.search(r'[A-ZĄČĘĖĮŠŲŪŽ]', str(first_name))
+    for ending in endings_array:
+        generated_last_name = f'{last_name_base}{ending}'
+        NAMES.append((f'{first_name} {generated_last_name}'), )
+        NAMES.append(f'{initials.group()}. {generated_last_name}')
+        NAMES.append(generated_last_name)
 
-    # last_name = re.sub(r'(' + '|'.join(MALE_LAST_NAME_ENDINGS) + r')$', random.choice(MALE_LAST_NAME_ENDINGS), last_name)
-
+def select_names_generator(first_name):
     if first_name.endswith(tuple(FEMALE_NAME_ENDINGS)):
-        print(f"{first_name}")
-    elif first_name.endswith(tuple(MALE_NAME_ENDINGS)):
-        print(f"{first_name}")
+        generate_names(FEMALE_LAST_NAME_ENDINGS, first_name)
+    elif first_name.endswith(tuple(MALE_LAST_NAME_ENDINGS)):
+        generate_names(MALE_LAST_NAME_ENDINGS, first_name)
 
-    # NAMES.append((f'{first_name} {last_name}'))
-    i += 1
+for i in range(10000):
+    select_names_generator(fake.first_name())
 
-# # Print generated names
-# for name in NAMES:
-#     print(name)
+# Print generated names
+for name in NAMES:
+    print(name)
+
+print(len(NAMES))
