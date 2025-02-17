e18b93a31a779a646b854bbb6fae2ae1146d397c
deimantejagu <deimajagu@outlook.com>
2025-02-17 12:57:41 +0200

    small fixes

--- src/main.py
+++ src/main.py
@@ -2,8 +2,8 @@ from model_training_and_testing import train_spacy
 from document_anonymization import anonymize_documents
 
 # Train model
-train_spacy("src/spaCy/best_model", "src/spaCy/best_optimizer.pkl", "src/dataset/train_dataset.json", "src/dataset/validation_dataset.json", 100, 16, 5)
+train_spacy("src/spaCy/model", "src/spaCy/optimizer.pkl", "src/dataset/train_dataset.json", "src/dataset/validation_dataset.json", 100, 16, 5)
 
 # Test model
 # Run document anonymization
-# anonymize_documents("src/documents", "src/processed_documents", "src/spaCy/best_model")
+# anonymize_documents("src/documents", "src/processed_documents", "src/spaCy/model")
