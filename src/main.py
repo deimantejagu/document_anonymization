85ee3c699fde78efbe5bd70a4f7cb35df5944177
deimantejagu <deimajagu@outlook.com>
2025-02-17 10:44:49 +0200

    generating dataset

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
-from utils import create_names_patterns
 from model_training_and_testing import train_spacy, test_spacy
 from document_anonymization import anonymize_documents
-
-# Uncomment if you need to generate new names with whom you will create new texts for model training dataset
-# names = create_names_patterns()
-# save_data("/NER/src/dataset/names.json", names)
 
 # Train model
 train_spacy("src/spaCy/best_model", "src/spaCy/best_optimizer.pkl", "src/dataset/train_dataset.json", 10)
